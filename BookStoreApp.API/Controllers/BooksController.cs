27d3d9f93f674658b2446d3f6be9dc954728d105
Juan Garcia <garciaju@kerndhs.com>
2023-02-07 13:36:45 -0800

    final stuff

--- BookStoreApp.API/Controllers/BooksController.cs
+++ BookStoreApp.API/Controllers/BooksController.cs
@@ -18,10 +18,12 @@ namespace BookStoreApp.API.Controllers {
     public class BooksController : ControllerBase {
         private readonly BookStoreDbContext _context;
         private readonly IMapper mapper;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public BooksController(BookStoreDbContext context, IMapper mapper) {
+        public BooksController(BookStoreDbContext context, IMapper mapper, IWebHostEnvironment webHostEnvironment) {
             _context = context;
             this.mapper = mapper;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         // GET: api/Books
@@ -81,7 +83,9 @@ namespace BookStoreApp.API.Controllers {
         [HttpPost]
         [Authorize(Roles = "Administrator")]
         public async Task<ActionResult<BookCreateDto>> PostBook(BookCreateDto bookDto) {
+
             var book = mapper.Map<Book>(bookDto);
+            book.Image = CreateFile(bookDto.ImageData, bookDto.OriginalImageName);
             _context.Books.Add(book);
             await _context.SaveChangesAsync();
 
@@ -101,6 +105,22 @@ namespace BookStoreApp.API.Controllers {
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        private string CreateFile(string imageBase64, string imageName) {
+
+            var url = HttpContext.Request.Host.Value;
+            var ext = Path.GetExtension(imageName);
+            var fileName = $"{Guid.NewGuid()}.{ext}";
+
+            var path = $"{webHostEnvironment.WebRootPath}\\bookcoverimages\\{fileName}";
+
+            byte[] image = Convert.FromBase64String(imageBase64);
+            var fileStream = System.IO.File.Create(path);
+            fileStream.Write(image, 0, image.Length);
+            fileStream.Close();
+
+            return $"https://{url}/bookcoverimage/{fileName}";
         }
 
         private async Task<bool> BookExistsAsync(int id) {
